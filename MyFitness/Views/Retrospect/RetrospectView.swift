e1680b5c84f1eb75abebd0b1de2171dca2be4bda
kanghun1121 <kanghun1121@naver.com>
2025-05-12 17:35:46 +0900

    CardioView 추가

---
+++ MyFitness/Views/Retrospect/RetrospectView.swift
@@ -0,0 +1,18 @@
+//
+//  Retrospect.swift
+//  MyFitness
+//
+//  Created by 강대훈 on 5/12/25.
+//
+
+import SwiftUI
+
+struct Retrospect: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Retrospect()
+}
