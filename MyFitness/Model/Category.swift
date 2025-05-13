0402af729ebdc1e9da5246b70db0660f627ce1a8
kanghun1121 <kanghun1121@naver.com>
2025-05-13 11:45:14 +0900

    stash

--- MyFitness/Model/Category.swift
+++ MyFitness/Model/Category.swift
@@ -7,12 +7,11 @@
 
 import Foundation
 
-enum Category: String, CaseIterable {
+enum Category: String, CaseIterable, Codable {
     case arm = "팔"
     case chest = "가슴"
     case back = "등"
     case shoulder = "어깨"
     case cardio = "유산소"
-    case leg = "다리"
-    case butt = "엉덩이"
+    case leg = "하체"
 }
