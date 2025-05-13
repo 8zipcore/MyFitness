b3be8abd02f8116d0391086ce5cd1129dba38cb4
kanghun1121 <kanghun1121@naver.com>
2025-05-14 00:42:56 +0900

    ViewModel 파일 생성 및 적용

---
+++ MyFitness/ViewModel/RetrospectWriteViewModel.swift
@@ -0,0 +1,71 @@
+import SwiftUI
+import SwiftData
+
+final class RetrospectWriteViewModel: ObservableObject {
+    @Published var retrospect: Retrospect
+    @Published var isInvalidDate: Bool = false
+    @Published var isInvalidExercise: Bool = false
+    @Published var categoryList: [Category] = Category.allCases
+
+    /// 최초 생성시에 사용되는 생성자입니다.
+    convenience init() {
+        self.init(retrospect: Retrospect(
+            date: Date.now,
+            category: [],
+            anaerobics: [],
+            cardios: [],
+            startTime: Date.now,
+            finishTime: Date.now,
+            satisfaction: 0,
+            writing: "",
+            bookMark: false
+        ))
+    }
+
+    /// 수정시에 사용되는 생성자입니다.
+    /// - Parameter retrospect: 기존에 존재하는 회고 데이터를 받아옵니다.
+    init(retrospect: Retrospect) {
+        self.retrospect = retrospect
+    }
+
+
+    /// 오후에서 오전으로 넘어가는 날에 하루가 추가되도록 합니다.
+    func checkPMtoAMTime() {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "a"
+        formatter.locale = Locale(identifier: "ko_kr")
+        let startDate = formatter.string(from: retrospect.startTime)
+        let finishDate = formatter.string(from: retrospect.finishTime)
+
+        if ((startDate == "오후" && finishDate == "오전") && !isValidDate()) {
+            if let tomorrow = Calendar.current.date(byAdding: .day, value: 1, to: retrospect.finishTime) {
+                retrospect.finishTime = tomorrow
+            }
+        }
+    }
+
+    /// 날짜 차이에 따라서 Bool값을 반환합니다.
+    func isValidDate() -> Bool {
+        return retrospect.startTime <= retrospect.finishTime
+    }
+
+    /// 운동 입력에서 입력을 하지 않음에 따라서 Bool값을 반환합니다.
+    func isValidExercise() -> Bool {
+        for anaerobic in retrospect.anaerobics {
+            if anaerobic.exercise.name.isEmpty { return false }
+        }
+
+        for cardio in retrospect.cardios {
+            if cardio.exercise.name.isEmpty { return false }
+        }
+
+        return true
+    }
+
+    /// 명시적으로 데이터를 저장합니다.
+    /// - Parameter context: DB를 조작할 수 있는 객체입니다.
+    func save(context: ModelContext) {
+        try? context.save()
+        print(retrospect.anaerobics.count, retrospect.cardios.count)
+    }
+}
